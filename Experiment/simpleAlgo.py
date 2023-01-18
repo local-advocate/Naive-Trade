8f539f890a512d9f4d0d9faa72c6fae4b3efe2e8
Ruchit Patel <ruchitnpatel@gmail.com>
2023-01-17 16:34:01 -0800

    algo profit something wrong, fix later, making graphs

--- Experiment/simpleAlgo.py
+++ Experiment/simpleAlgo.py
@@ -7,7 +7,6 @@ np.set_printoptions(precision=2)
 # array to store all the data
 data = []
 profitArr = []
-algoProfit = 0
 invest = 10000          # initial investment
 buyPrice = 0
 sellPrice = 0
@@ -16,18 +15,17 @@ shares = 0
 
 # generate random floats array of n points
 def generate_data(fromm, to, n):
-    global data, algoProfit, profitArr
+    global data, profitArr
     data = (fromm-to) * np.random.random(n) + to
 
 def algo(n):
     global data, algoInvest, shares
     tracker = data[0]
-    buyPrice = data[0]
     sold = False
     shares = algoInvest / data[0]                   # Initial shares bought @ data[0]
     i = 1
     pArr = []
-    while (i < n):
+    while (i < n-1):
         curr = data[i]
         tracker = data[i-1]
         # If the latest data is less than previous data point
@@ -36,7 +34,7 @@ def algo(n):
             if not sold:
                 #print('S-{0}-{1}'.format(i, curr))  
                 algoInvest = shares*curr            # Available to invest = shares * current price
-                print('SOLD {} shares @ {}. Total available: {}'.format(shares, curr, algoInvest))
+                shares = 0
                 sold = True
                 profitArr.append(pArr)
                 pArr = []
@@ -48,7 +46,7 @@ def algo(n):
             if sold:
                 #print('B-{0}-{1}'.format(i, curr))
                 shares = algoInvest/curr            # Total shares = Funds available / current price
-                print('Bought {} shares @ {}. Total available: {}'.format(shares, curr, algoInvest))
+                algoInvest = 0
                 sold = False
         i += 1
     return
@@ -58,11 +56,14 @@ def print_stats(n):
     print('Profit array: ', profitArr)
     print('Initial investment($) {}'.format(invest))
     print('Total profit($) {:.2f}'.format(((invest/data[0])*data[n-1])-invest))
-    print('Algo profit($) {:.2f}'.format((shares*data[n-1])-invest))
+    if (shares == 0):
+        print('Algo profit($) {:.2f}'.format(algoInvest-invest))
+    else:
+        print('Algo profit($) {:.2f}'.format((shares*data[n-1])-invest))
     return
     
 if __name__ == '__main__':
-    fromm, to, n = 90, 100, 20
+    fromm, to, n = 0, 100, 1000
     endPrice = (fromm-to) * np.random.random() + to
     generate_data(fromm, to, n)
     algo(n)
