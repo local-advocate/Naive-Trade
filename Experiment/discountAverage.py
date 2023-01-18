57fd1966fd93ac1916cf195cfdb10ee1728a7391
Ruchit Patel <ruchitnpatel@gmail.com>
2023-01-18 15:31:16 -0800

    file organiztion

--- Experiment/discountAverage.py
+++ Experiment/discountAverage.py
@@ -3,6 +3,7 @@
 import numpy as np
 import yfinance as yf
 import matplotlib.pyplot as plt
+from DiscountedAveragerator import DiscountedAveragerator
 
 data = []
 averageArr = []
@@ -11,29 +12,6 @@ algoInvest = invest
 shares = 0
 soldArr = []
 boughtArr = []
-
-# A smoother and effiecient implementation of sliding window average.
-class DiscountedAveragerator:
-    def __init__(self, alpha):      # lower the alpha, the less weight of the previous values
-        self.alpha = alpha
-        self.w = 0.
-        self.sum_x = 0.
-        self.sum_x_sq = 0.
-
-    def add(self, x):
-        self.w = self.alpha * self.w + 1.
-        self.sum_x = self.alpha * self.sum_x + x
-        self.sum_x_sq = self.alpha * self.sum_x_sq + x * x
-
-    @property
-    def avg(self):
-        return self.sum_x / self.w
-
-    @property
-    def std(self):
-        mu = self.avg
-        # The np.maximum is purely for safety.
-        return np.sqrt(np.maximum(0., self.sum_x_sq / self.w - mu * mu))
 
 # Same algorithm
 def algo(n):
