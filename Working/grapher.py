c794c6e0b75bb982c1b705b870b9aad4b2df62f1
Ruchit Patel <ruchitnpatel@gmail.com>
2023-01-19 10:49:43 -0800

    done moving Average

--- Working/grapher.py
+++ Working/grapher.py
@@ -15,8 +15,8 @@ class Grapher:
         if len(self.deviation) != 0: plt.plot(self.deviation, color='y', ls='dashed')             # plot average
         if len(self.sell) != 0:                                                                   # plot sell
             for xc in self.sell:
-                plt.plot(xc, self.data[xc], 'ro', markersize=2)
+                plt.plot(xc, self.data[xc], 'ro', markersize=4)
         if len(self.buy) != 0:                                                                    # plot buy
             for xc in self.buy:
-                plt.plot(xc, self.data[xc], 'go', markersize=2)
+                plt.plot(xc, self.data[xc], 'go', markersize=4)
         plt.show()
--- Working/movingAverage.py
+++ Working/movingAverage.py
@@ -5,28 +5,72 @@ from grapher import Grapher
 
 # The moving average algorithm
 class MovingAverage():
-    def __init__(self, data, alpha):
+    def __init__(self, data, alpha, invest):
         self.data = data
-        self.averagerator = DiscountedAveragerator(0.8)
+        self.averagerator = DiscountedAveragerator(alpha=alpha)
         self.sell = []
         self.buy = []
         self.average = []
+        self.shares = 0
+        self.invest = invest
+        self.algoInvest = self.invest
     
     def algo(self):
-        print('MA Algo')
-        for d in self.data:
-            self.averagerator.add(d)
-            self.average.append(self.averagerator.avg)
+        # Compare against data[0]
+        self.averagerator.add(self.data[0])
+        self.average.append(self.averagerator.avg)
+        average = self.averagerator.avg                 # Ongoing average value
+        
+        # Initialize variables
+        bought = False
+        i = 1
+        n = len(self.data)
+        
+        while (i < n-1):
+            curr = self.data[i]
+            # calc new average
+            self.averagerator.add(curr)
+            average = self.averagerator.avg
+            self.average.append(average)
+            
+            # if curr price < average
+            if (curr < average):
+                # we have not bought the shares, buy
+                if not bought:
+                    self.shares = self.algoInvest/curr            # Total shares = Funds available / current price
+                    self.algoInvest = 0
+                    bought = True
+                    self.buy.append(i)
+            # curr price > average
+            else:
+                # if we bought it, sell
+                if bought:
+                    self.algoInvest = self.shares*curr            # Available to invest = shares * current price
+                    self.shares = 0
+                    bought = False
+                    self.sell.append(i)
+            i += 1
+        return
 
     def stats(self):
-        print('MA Stats')
+        print('Initial investment($) {}'.format(self.invest))
+        # Buy at the beginning, self at the end price
+        print('Total profit($) {:.2f}'.format(((self.invest/self.data[0])*self.data[-1])-self.invest))
+        # All shares sold, remaining value v. what we started with
+        if (self.shares == 0):
+            print('Algo profit($) {:.2f}'.format(self.algoInvest-self.invest))
+        # Shares remaining, sold at the end price
+        else:
+            print('Algo profit($) {:.2f}'.format((self.shares*self.data[-1])-self.invest))
+            self.sell.append(len(self.data)-1)
 
 if __name__ == '__main__':
-    # Company Info
+    # Company and Investment Info
     info = {
         'company': 'AMZN',
         'period' : '1d',
-        'interval': '1m'
+        'interval': '1m',
+        'invest': 10000
     }
     
     # Collect data
@@ -34,10 +78,10 @@ if __name__ == '__main__':
     collector.gather()
     
     # Run Algo & Print Stats
-    mavg = MovingAverage(data=collector.data, alpha=0.8)
+    mavg = MovingAverage(data=collector.data, alpha=0.8, invest=info['invest'])
     mavg.algo()
     mavg.stats()
     
     # Graph the results
-    grapher = Grapher(data=collector.data, average=mavg.average)
+    grapher = Grapher(data=collector.data, average=mavg.average, sell=mavg.sell, buy=mavg.buy)
     grapher.graphit()
