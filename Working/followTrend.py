53b0afa1dd73dde67a893277a8f66b465163bf9b
Ruchit Patel <ruchitnpatel@gmail.com>
2023-01-19 17:00:23 -0800

    resoruce

--- Working/followTrend.py
+++ Working/followTrend.py
@@ -1,1 +1,3 @@
 # Make trades looking at trends (sell if downwards, buy if upwards).
+# https://en.wikipedia.org/wiki/Linear_trend_estimation
+# https://stackoverflow.com/questions/20329545/how-to-check-if-a-sequence-of-numbers-has-a-increasing-decreasing-trend-in-c
